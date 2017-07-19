b75914287d4bfb81c09478c21beb205d624a9b8b
Markus Tran <thebluepillow@gmail.com>
2017-07-19 11:19:14 -0700

    missing deps

--- src/tlmc-serve.js
+++ src/tlmc-serve.js
@@ -13,56 +13,58 @@ const TLMC_PATH = process.argv[3] || process.env.TLMC_PATH || '/mnt/TouhouBox/tl
 const LS_CACHE_PATH = path.join(__dirname, '_ls.cache')
 const CUE_CACHE_PATH = path.join(__dirname, '_cue.cache')
 
-// Initialize `ls-cache`
-try {
-  fs.accessSync(LS_CACHE_PATH)
-  console.log('ls-cache found. Using cache.')
-} catch (err) {
-  console.log('No ls-cache found. Creating directory structure...')
-  const directoryString = ls.serialize(TLMC_PATH)
-  fs.writeFileSync(LS_CACHE_PATH, directoryString)
-  console.log(`ls-cache created at ${LS_CACHE_PATH}`)
-  console.log()
-}
-
-// Initialize `cue-cache`
-try {
-  fs.accessSync(CUE_CACHE_PATH)
-  console.log('cue-cache found. Using cache.')
-} catch (err) {
-  console.log('No cue-cache found. Reading directory structure...')
-  const directory = ls.deserialize(fs.readFileSync(LS_CACHE_PATH).toString())
-  const songs = readCues(directory, TLMC_PATH)
-  const songsString = csvStringify(songs, { header: true })
-  fs.writeFileSync(CUE_CACHE_PATH, songsString)
-  console.log(`cue-cache created at ${CUE_CACHE_PATH}`)
-  console.log()
-}
-
-// Check songs found
-console.log('Checking file paths...')
-const directory = ls.deserialize(fs.readFileSync(LS_CACHE_PATH).toString())
-const songs = csvParse(fs.readFileSync(CUE_CACHE_PATH).toString(), { columns: true })
-let failed = songs.filter(song => {
-  let parts = song.path.split(path.sep)
-  for (let index = 0, currDir = directory; index < parts.length - 1; ++index) {
-    currDir = Array.from(currDir.files)[parts[index]]
-    parts[index] = currDir.base
-  }
-
+{
+  // Initialize `ls-cache`
   try {
-    fs.accessSync(path.join(TLMC_PATH, ...parts))
-    return false
+    fs.accessSync(LS_CACHE_PATH)
+    console.log('ls-cache found. Using cache.')
   } catch (err) {
-    console.log(`- ${path.join(...parts)}`)
-    return true
+    console.log('No ls-cache found. Creating directory structure...')
+    const directoryString = ls.serialize(TLMC_PATH)
+    fs.writeFileSync(LS_CACHE_PATH, directoryString)
+    console.log(`ls-cache created at ${LS_CACHE_PATH}`)
+    console.log()
   }
-})
 
-if (failed.length) {
-  console.log(`${failed.length} songs not found.`)
-} else {
-  console.log('All OK!')
+  // Initialize `cue-cache`
+  try {
+    fs.accessSync(CUE_CACHE_PATH)
+    console.log('cue-cache found. Using cache.')
+  } catch (err) {
+    console.log('No cue-cache found. Reading directory structure...')
+    const directory = ls.deserialize(fs.readFileSync(LS_CACHE_PATH).toString())
+    const songs = readCues(directory, TLMC_PATH)
+    const songsString = csvStringify(songs, { header: true })
+    fs.writeFileSync(CUE_CACHE_PATH, songsString)
+    console.log(`cue-cache created at ${CUE_CACHE_PATH}`)
+    console.log()
+  }
+
+  // Check songs found
+  console.log('Checking file paths...')
+  const directory = ls.deserialize(fs.readFileSync(LS_CACHE_PATH).toString())
+  const songs = csvParse(fs.readFileSync(CUE_CACHE_PATH).toString(), { columns: true })
+  let failed = songs.filter(song => {
+    let parts = song.path.split(path.sep)
+    for (let index = 0, currDir = directory; index < parts.length - 1; ++index) {
+      currDir = Array.from(currDir.files)[parts[index]]
+      parts[index] = currDir.base
+    }
+
+    try {
+      fs.accessSync(path.join(TLMC_PATH, ...parts))
+      return false
+    } catch (err) {
+      console.log(`- ${path.join(...parts)}`)
+      return true
+    }
+  })
+
+  if (failed.length) {
+    console.log(`${failed.length} songs not found.`)
+  } else {
+    console.log('All OK!')
+  }
 }
 
 // Create server
