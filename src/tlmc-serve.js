27f5d13cd5d06613aa7679b1a9f394d54888340b
Markus Tran <thebluepillow@gmail.com>
2020-07-24 01:28:32 -0700

    remove 401

--- src/tlmc-serve.js
+++ src/tlmc-serve.js
@@ -12,13 +12,6 @@ require('./ls-cache')(PATHS)
 // Create server
 const app = express()
 
-app.use((req, res, next) => {
-  if (process.env.NODE_ENV === 'production' && req.ip !== '138.197.197.88') {
-    return res.sendStatus(401)
-  }
-  next()
-})
-
 app.use(compression({ level: 9 }))
 app.use('/tlmc', express.static(PATHS.tlmc))
 app.get('/ls', (req, res) => { res.sendFile(PATHS.lsCache) })
