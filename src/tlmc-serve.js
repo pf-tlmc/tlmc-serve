10af39766863bbf0a10520ba95a1de6133827fe7
Markus Tran <thebluepillow@gmail.com>
2020-07-30 23:59:13 -0700

    serve index

--- src/tlmc-serve.js
+++ src/tlmc-serve.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express')
+const serveIndex = require('serve-index')
 const compression = require('compression')
 const mime = require('mime')
 const createThumbnail = require('./create-thumbnail')
@@ -14,8 +15,8 @@ require('./ls-cache')(PATHS)
 
 // Create server
 const app = express()
-
 app.use(compression({ level: 9 }))
+
 app.use('/tlmc/**', async (req, res, next) => {
   const filePath = req.params[0]
   const ext = path.extname(filePath)
@@ -31,7 +32,9 @@ app.use('/tlmc/**', async (req, res, next) => {
     next()
   }
 })
-app.use('/tlmc', express.static(PATHS.tlmc))
+
+app.use('/tlmc', express.static(PATHS.tlmc), serveIndex(PATHS.tlmc, { icons: true }))
+
 app.get('/ls', (req, res) => { res.sendFile(PATHS.lsCache) })
 
 app.use((req, res) => { res.sendStatus(404) })
